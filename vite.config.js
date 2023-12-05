a088a84144a146aff84f1c0e318b6f044a163a8a
Nikolay Shein <qseboy1998@gmail.com>
2023-12-05 22:01:53 +0200

    change basename to config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hacker-news-front/",
+  base: "/hacker-news-app/",
 });
